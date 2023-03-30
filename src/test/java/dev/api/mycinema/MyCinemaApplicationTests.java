d4562cc255fd0b6b18c6def50a32aa15445339f1
gustavojms <gustavo.joset0@hotmail.com>
2023-03-29 23:30:55 -0300

    Initial commit

---
+++ src/test/java/dev/api/mycinema/MyCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.api.mycinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
